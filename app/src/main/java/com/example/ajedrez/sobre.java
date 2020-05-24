365e0dc4c31c2189874d565dd46116bd0ae3f4a7
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-05-24 23:53:14 +0200

    SOBRE

---
+++ app/src/main/java/com/example/ajedrez/sobre.java
@@ -0,0 +1,21 @@
+package com.example.ajedrez;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * <h1>Ayuda</h1>
+ * Aqui damos informacion sobre las normas del juego y etc
+ *
+ * @author  Jeremy, Nestor, Hector
+ * @version 1.0
+ * @since   24-05-2020
+ */
+public class sobre extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre);
+    }
+}
